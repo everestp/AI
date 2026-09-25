b182424f9472fe805a12252284b653139b0d1c2a
Everest Paudel <145990676+everestp@users.noreply.github.com>
2026-09-25 19:07:39 +0545

    Fix

--- rag_project_chaicode/worker.sh
+++ rag_project_chaicode/worker.sh
@@ -1,1 +1,1 @@
- rq worker --with-scheduler --url rediss://default:gQAAAAAABIGIAAIgcDJmYmNiNGFhOWQ2YWQ0M2MyYmEzOTU4OGU4YTYzYjZlYQ@crack-polliwog-295304.upstash.io:6379
+ rq worker --with-scheduler --url <REDIS_URL>
